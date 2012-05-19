50963381d1a459e757c07074bbc8832be0deda9d
Mark Hayes <mark@zurb.com>
2012-05-19 16:24:42 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wherefolio::Application.config.secret_token = '8dc2723b282c27054fec8e5574e45e03e90c43d508071ad488d0f3aa757e78c40c4af37eb849a401dcd9bcfab8112c184141f2f94c0410074b117bdd0aef5d67'
