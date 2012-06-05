b1845ef8d6e9247abcef2279ed450c3004d37df6
Mark Hayes <mark@deployfx.com>
2012-06-04 22:18:07 -0700

    updated navigation menu

--- test/integration/signin_test.rb
+++ test/integration/signin_test.rb
@@ -1,17 +1,24 @@
 require "test_helper"
 
-class SigninTest < IntegrationTest
-  setup do
-    @user = FactoryGirl.create(:user)
-    login_as(@user)
+class SigninTest < IntegrationTest  
+  it "should provide an option to signin" do
+    visit root_path
+    current_path.must_equal root_path
+    
+    page.must_have_selector ".navbar", text: "Sign In"
+    page.wont_have_selector ".dropdown-menu", text: "Account"
+    page.wont_have_selector ".dropdown-menu", text: "Sign Out"
   end
   
   it "should allow a user to signin" do
+    @user = FactoryGirl.create(:user)
+    login_as(@user)
+    
     visit root_path
     current_path.must_equal root_path
     
     page.must_have_selector ".navbar", text: @user.name
-    page.must_have_selector ".navbar", text: "Account"
-    page.must_have_selector ".navbar", text: "Sign Out"
+    page.must_have_selector ".dropdown-menu", text: "Account"
+    page.must_have_selector ".dropdown-menu", text: "Sign Out"
   end
 end
