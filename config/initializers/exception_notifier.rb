b2bba65bbdcc96d38a2769cf35c653e38e6df1cc
Mark Hayes <mark@deployfx.com>
2012-06-07 00:20:57 -0700

    fix typo

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  Transpireapp::Application.config.middleware.use ExceptionNotifier,
+  Wherefolio::Application.config.middleware.use ExceptionNotifier,
     email_prefix: "[WHEREFOLIO] ",
     sender_address: %{"exceptions" <no-reply@wherefolio.com>},
     exception_recipients: 'mark@deployfx.com'
